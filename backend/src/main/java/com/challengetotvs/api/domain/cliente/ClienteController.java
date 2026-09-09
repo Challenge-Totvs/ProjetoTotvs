5fb50a42c3340922b5ba942b1d68109efd308a82
Kelwin <kelwin.sbastos21@gmail.com>
2026-09-09 09:34:36 -0300

    feat: add the list method in ClienteController

--- backend/src/main/java/com/challengetotvs/api/domain/cliente/ClienteController.java
+++ backend/src/main/java/com/challengetotvs/api/domain/cliente/ClienteController.java
@@ -2,6 +2,9 @@ package com.challengetotvs.api.domain.cliente;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +17,15 @@ public class ClienteController {
     private final ClienteService service;
 
     @PostMapping("/register")
-    public ResponseEntity<Void> criar(@RequestBody @Valid ClienteRequest request){
+    public ResponseEntity<Void> create(@RequestBody @Valid ClienteRequest request){
         service.criar(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ClienteResponse>> listall(@PageableDefault(size = 10, sort = {"nome"})Pageable pagination){
+        var cliente = service.listar(pagination);
+        return ResponseEntity.ok(cliente);
+    }
+
 }
