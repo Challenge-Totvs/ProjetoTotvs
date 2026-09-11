444907ba784f4b9648746b88341c872b5c6909be
Kelwin <kelwin.sbastos21@gmail.com>
2026-09-11 10:56:23 -0300

    feat(domain): add TranscricaoRepository extending JpaRepository

---
+++ backend/src/main/java/com/challengetotvs/api/domain/transcricao/TranscricaoRepository.java
@@ -0,0 +1,6 @@
+package com.challengetotvs.api.domain.transcricao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TranscricaoRepository extends JpaRepository<Transcricao, Long> {
+}
